283304f8cb5d24e4a32b3589213433eea00e8811
stolkachev <stolkachev@gmail.com>
2018-05-26 22:54:41 -0500

    NNOD Editor 0.31

--- js/editor_demo.js
+++ js/editor_demo.js
@@ -1,4 +1,3 @@
-var u_id = 1;
 var get_Neurons_Url = "/slq_get_neurons.php?u_id=" + u_id;
 var get_Nerves_Url = "/slq_get_nerves.php?u_id=" + u_id;
 var update_Node_Coordinates_Url = "/slq_update_Node_Coordinates_d.php?u_id=" + u_id;
